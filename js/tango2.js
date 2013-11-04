a96b0d2475aa24c26322c25369c14724d144ad10
jpbbroncos53 <admin@wireless148-121.wireless.wmich.edu>
2013-11-04 15:29:02 -0500

    sep files for testing

--- js/tango2.js
+++ js/tango2.js
@@ -5,33 +5,7 @@ function preload(url) {
 	return img
 }
 
-function getRandomColor() {
-		return colors[Math.round(Math.random() * 5)];
-}
 
-function tango(layer) {
-
-	for(var n = 0; n < layer.getChildren().length; n++) {
-		var color = Kinetic.Util.getRGB(getRandomColor());
-		var shape = layer.getChildren()[n];
-		var stage = shape.getStage();
-		var radius = Math.random() * 100 + 20;
-		
-		new Kinetic.Tween({
-			node: shape, 
-			duration: 1,
-			x: Math.random() * stage.getWidth(), 
-			y: Math.random() * stage.getHeight(), 
-			rotation: Math.random() * Math.PI * 2, 
-			radius: radius,
-			opacity: (radius - 20) / 100,
-			easing: Kinetic.Easings.EaseInOut,
-			fillR: color.r,
-			fillG: color.g,
-			fillB: color.b
-		}).play();
-	}
-}
 
 function remove(layer) {
 	for(var n = 0; n < layer.getChildren().length; n++) {
@@ -79,13 +53,19 @@ function create(layer) {
 		y: Math.random() * stage.getHeight(),
 		sides: Math.ceil(Math.random() * 5 + 3),
 		radius: radius,
-		fill: getRandomColor(),
+		fill: 'red',
 		opacity: (radius - 20) / 100,
 		draggable: true
 	});
 
 	layer.add(shape);
 	stage.draw();
+}
+
+function addline(layer){
+	
+
+	
 }
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
@@ -110,22 +90,7 @@ var stage = new Kinetic.Stage({
 var layer = new Kinetic.Layer();
 stage.add(layer);
 
-// Create 10 random shapes
-for(var n = 0; n < 10; n++) {
-	var radius = Math.random() * 100 + 20;
-	var shape = new Kinetic.RegularPolygon({
-		x: Math.random() * stage.getWidth(),
-		y: Math.random() * stage.getHeight(),
-		sides: Math.ceil(Math.random() * 5 + 3),
-		radius: radius,
-		fill: getRandomColor(),
-		opacity: (radius - 20) / 100,
-		draggable: true
-	});
 
-	// Add each shape to the layer
-	layer.add(shape);
-}
 
 // Redraw the stage
 stage.draw();
